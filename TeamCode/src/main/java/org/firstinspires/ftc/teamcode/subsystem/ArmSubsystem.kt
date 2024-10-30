6e02cf76b4c7a5b887be445c7248eb6993e682b9
Upsidedly <matthewlloydw@gmail.com>
2024-10-30 10:08:54 -0500

    chore: add comment for suppression decorator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/ArmSubsystem.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/ArmSubsystem.kt
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.subsystem
 
 import com.arcrobotics.ftclib.command.SubsystemBase
 
+// Leave the suppression until we integrate... Android studio will complain
 @Suppress("unused")
 class ArmSubsystem : SubsystemBase() {
 //    override fun periodic() {
