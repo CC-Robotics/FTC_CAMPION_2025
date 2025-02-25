5a908b51e90ddbbfcace6caabe05a310b711f657
Upsidedly <matthewlloydw@gmail.com>
2025-02-25 15:24:21 -0500

    feat: add backup run to position systems in case of pidf failure

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/LiftRTPSubsystem.kt
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.subsystem
+
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorEx
+import dev.frozenmilk.dairy.core.dependency.Dependency
+import dev.frozenmilk.dairy.core.dependency.annotation.SingleAnnotation
+import dev.frozenmilk.dairy.core.wrapper.Wrapper
+import dev.frozenmilk.mercurial.Mercurial
+import dev.frozenmilk.mercurial.subsystems.Subsystem
+import org.firstinspires.ftc.teamcode.utils.applySensitivity
+import org.firstinspires.ftc.teamcode.structures.PIDFSubsystem
+import java.lang.annotation.Inherited
+
+object LiftRTPSubsystem : PIDFSubsystem() {
+    @Target(AnnotationTarget.CLASS)
+    @Retention(AnnotationRetention.RUNTIME)
+    @MustBeDocumented
+    @Inherited
+    annotation class Attach
+
+    override var dependency: Dependency<*> = Subsystem.DEFAULT_DEPENDENCY and
+            SingleAnnotation(Attach::class.java)
+
+    override val subsystemName = "Lift"
+
+    private val rightLift by getHardware<DcMotorEx>("right_lift")
+    private val leftLift by getHardware<DcMotorEx>("left_lift")
+
+    const val MAX_POSITION = 150
+
+    override fun init(opMode: Wrapper) {
+        position = 0
+        leftLift.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        rightLift.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        leftLift.mode = DcMotor.RunMode.RUN_TO_POSITION
+        rightLift.mode = DcMotor.RunMode.RUN_TO_POSITION
+    }
+
+    fun update(increment: Double) {
+        changePosition(
+            applySensitivity(increment, 1.0, 0.2)
+        )
+        clampPosition(0, MAX_POSITION)
+        leftLift.targetPosition = position
+        rightLift.targetPosition = position
+        telemetry()
+    }
+
+    fun telemetry() {
+        telemetry.addData("Left Stick Y", Mercurial.gamepad2.leftStickY.state)
+        telemetry.addData("Left Lift Real Position", leftLift.currentPosition)
+        telemetry.addData("Right Lift Real Position", rightLift.currentPosition)
+        telemetry.addData("$subsystemName Position", position)
+        telemetry.addData("Left Lift Power (real vs intended)", "${leftLift.power}")
+        telemetry.addData("Right Lift Power (real vs intended)", "${rightLift.power}")
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/LinearSlideRTPSubsystem.kt
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.subsystem
+
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorEx
+import com.qualcomm.robotcore.hardware.DcMotorSimple
+import dev.frozenmilk.dairy.core.dependency.Dependency
+import dev.frozenmilk.dairy.core.dependency.annotation.SingleAnnotation
+import dev.frozenmilk.dairy.core.wrapper.Wrapper
+import dev.frozenmilk.mercurial.subsystems.Subsystem
+import org.firstinspires.ftc.teamcode.structures.PIDFSubsystem
+import java.lang.annotation.Inherited
+
+object LinearSlideRTPSubsystem : PIDFSubsystem() {
+    @Target(AnnotationTarget.CLASS)
+    @Retention(AnnotationRetention.RUNTIME)
+    @MustBeDocumented
+    @Inherited
+    annotation class Attach
+
+    override var dependency: Dependency<*> = Subsystem.DEFAULT_DEPENDENCY and
+            SingleAnnotation(Attach::class.java)
+
+    override val subsystemName = "Slide"
+
+    private val slide by getHardware<DcMotorEx>("slide")
+
+    private const val MAX_POSITION = 600
+
+    override fun init(opMode: Wrapper) {
+        position = 0
+        slide.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        slide.mode = DcMotor.RunMode.RUN_TO_POSITION
+        slide.direction = DcMotorSimple.Direction.REVERSE
+    }
+
+    fun update(increment: Double) {
+        changePosition(increment)
+        clampPosition(0, MAX_POSITION)
+        slide.targetPosition = MAX_POSITION
+        telemetry()
+    }
+
+    fun telemetry() {
+        telemetry.addData("$subsystemName Real Position", slide.currentPosition)
+        telemetry.addData("$subsystemName Position", position)
+        telemetry.addData("$subsystemName Power", "${slide.power}")
+    }
+
+    enum class SlidePosition(val position: Int) {
+        VISION_POSITION(300)
+    }
+}
